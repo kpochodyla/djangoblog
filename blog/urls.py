50fbcaf1e6952cd70da91226aea8248a91469248
Krystian PC <kpochodyla@protonmail.com>
2021-08-23 16:18:44 +0200

    Obłsuga plików static przy wyłączonym debugowaniu

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,14 @@
-from django.urls import path
+from django.urls import path, include, re_path
+from django.conf.urls.static import static, serve
 from . import views
+from mysite import settings
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+
 ]
